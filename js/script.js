ad0b404565d6c14c6f02c751b9fac5ffbabb931e
Graalex <41749146+hazed0m@users.noreply.github.com>
2019-06-24 13:00:35 +0300

    refresh

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
+var autoHeight = $('.block-wrapper > div').height();
 $('.block-wrapper .reklama-block, .block-wrapper .anim-block, .block-wrapper .threed-block').hover(
-    function(){
+    function(){    
+        let currentH = $(this).height();
+        $(this).animate({height:currentH+100},100); 
+        $(this).find('.button-block').delay(300).fadeIn('fast');
         setTimeout(() => {
             $(this).find('video').trigger('play');
-        },200);   
+        },200);
     },
-    function(){
-        $(this).find('video').trigger('pause');
+    function(){       
+        let currentH = $(this).height(); 
+        $(this).delay(200).animate({height:autoHeight},100);
+        $(this).find('.button-block').fadeOut('fast');
         setTimeout(() => {
-            $(this).find('video').trigger('load');
+            $(this).find('video').trigger('pause');
         },200);        
     }
 );
