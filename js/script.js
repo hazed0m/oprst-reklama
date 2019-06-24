01e24cc2939791025a4a3bccad8987cd36af685f
Graalex <41749146+hazed0m@users.noreply.github.com>
2019-06-24 15:07:16 +0300

    add

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
-var autoHeight = $('.block-wrapper .inner-wrap > div').height();
+var autoHeight = $('.block-wrapper .inner-wrap > div').height(),
+    catalizator = 80;
 $(window).resize(function(){
     autoHeight = $('.block-wrapper .inner-wrap > div').height();
 });
 $('.block-wrapper .reklama-block, .block-wrapper .anim-block, .block-wrapper .threed-block').hover(
-    function(){    
-        let currentH = $(this).height();
-        $(this).animate({height:currentH+70},100); 
+    function(){          
+        $(this).find('.button-block').stop();
+        let currentH = 0;
+        if($(window).width()<=450)
+        {
+            currentH = parseInt($(this).height());
+        }
+        else
+        {
+            currentH = parseInt($(this).css('min-height'));
+        }
+        $(this).animate({height:currentH+catalizator},100); 
         $(this).find('.button-block').delay(300).fadeIn('fast');
         setTimeout(() => {
             $(this).find('video').trigger('play');
@@ -16,7 +26,7 @@ $('.block-wrapper .reklama-block, .block-wrapper .anim-block, .block-wrapper .th
         $(this).delay(200).animate({height:autoHeight},100);
         $(this).find('.button-block').fadeOut('fast');
         setTimeout(() => {
-            $(this).find('video').trigger('pause');
-        },200);        
+            $(this).find('video').trigger('pause');         
+        },200);     
     }
 );
